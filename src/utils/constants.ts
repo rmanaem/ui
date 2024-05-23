86225b4eb5ba98cdeb5824bc3a141ce3f98fee0c
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2024-05-23 14:18:10 -0400

    [MNT] Renamed `uploader` to `upload` everywhere (#18)
    * Renamed `uploader` to `upload` everywhere
    
    * Updated README.md

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const APIURL: string = import.meta.env.NB_OPENNEURO_UPLOADER_API;
+const APIURL: string = import.meta.env.NB_OPENNEURO_UPLOAD_API;
 export const updateURL: string = APIURL.endsWith('/')
   ? `${APIURL}openneuro/upload?dataset_id`
   : `${APIURL}/openneuro/upload?dataset_id`;
