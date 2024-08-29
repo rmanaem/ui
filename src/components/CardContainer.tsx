4f57f57154fc96ba6754c0b18e4f19ebf9a05da0
rmanaem <rmanaem@protonmail.ch>
2024-08-29 12:58:25 -0400

    Added instructions dialog and number of available datasets

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import { useState, useMemo } from 'react';
+import { Typography, IconButton } from '@mui/material';
+import HelpIcon from '@mui/icons-material/Help';
 import RepoCard from './RepoCard';
+import InstructionDialog from './InstructionDialog';
 import { RepoInfo } from '../utils/types';
 
 function CardContainer({
@@ -11,8 +14,10 @@ function CardContainer({
   nameFilters: string[];
   statusFilters: string[];
 }) {
+  const [openDialog, setOpenDialog] = useState(false);
+
   // Memoized function to filter repositories
-  const filteredRepos = React.useMemo(() => {
+  const filteredRepos = useMemo(() => {
     return repos.filter((repo) => {
       // Check if the repo name matches any of the name filters
       const nameMatch = nameFilters.length === 0 || nameFilters.includes(repo.name);
@@ -36,16 +41,31 @@ function CardContainer({
   }, [repos, nameFilters, statusFilters]); // Only recompute when these dependencies change
 
   return (
-    <div className="h-[72vh] space-y-4 overflow-y-auto">
-      {filteredRepos.map((repo) => (
-        <RepoCard
-          key={repo.name}
-          repoName={repo.name}
-          tsvExists={repo.tsv_exists}
-          jsonExists={repo.json_exists}
-          annotated={repo.annotated}
-        />
-      ))}
+    <div className="grid grid-rows-1">
+      <div className="grid grid-cols-2 items-center justify-items-center">
+        <div className="justify-self-start">
+          <Typography variant="body1">
+            Number of available repositories: {filteredRepos.length}
+          </Typography>
+        </div>
+        <div className="justify-self-end">
+          <IconButton component="button" color="primary" onClick={() => setOpenDialog(true)}>
+            <HelpIcon fontSize="large" />
+          </IconButton>
+          <InstructionDialog open={openDialog} onClose={() => setOpenDialog(false)} />
+        </div>
+      </div>
+      <div className="h-[72vh] space-y-4 overflow-y-auto">
+        {filteredRepos.map((repo) => (
+          <RepoCard
+            key={repo.name}
+            repoName={repo.name}
+            tsvExists={repo.tsv_exists}
+            jsonExists={repo.json_exists}
+            annotated={repo.annotated}
+          />
+        ))}
+      </div>
     </div>
   );
 }
