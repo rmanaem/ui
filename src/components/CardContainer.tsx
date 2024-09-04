3063180d54256f3de64f8f336af840b2b258f265
rmanaem <rmanaem@protonmail.ch>
2024-09-04 14:10:45 -0400

    Refactored `CardContainer` component

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -48,15 +48,15 @@ function CardContainer({
     <div className="grid grid-rows-1">
       <div className="grid grid-cols-2 items-center justify-items-center">
         <div className="justify-self-start">
+          <IconButton component="button" color="primary" onClick={() => setOpenDialog(true)}>
+            <HelpIcon fontSize="medium" />
+          </IconButton>
+          <InstructionDialog open={openDialog} onClose={() => setOpenDialog(false)} />
+        </div>
+        <div className="justify-self-end">
           <Typography variant="body1">
             Number of available repositories: {filteredRepos.length}
           </Typography>
-        </div>
-        <div className="justify-self-end">
-          <IconButton component="button" color="primary" onClick={() => setOpenDialog(true)}>
-            <HelpIcon fontSize="large" />
-          </IconButton>
-          <InstructionDialog open={openDialog} onClose={() => setOpenDialog(false)} />
         </div>
       </div>
       <div className="h-[72vh] space-y-4 overflow-y-auto">
