c36eb107a72ff654e657a84692e9f91c4b53e48c
rmanaem <rmanaem@protonmail.ch>
2024-09-04 14:10:14 -0400

    Implemented form for upload workflow

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,5 +1,7 @@
-import { TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import { TextField, Button, Typography, Alert, IconButton, Collapse } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import HelpIcon from '@mui/icons-material/Help';
 
 function Upload({
   fileInput,
@@ -14,6 +16,8 @@ function Upload({
   setUploadedFile: (value: File | null) => void;
   onHandleSubmit: () => void;
 }) {
+  const [showAlert, setShowAlert] = useState(true);
+
   function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
     const file = event.target.files?.[0] || null;
     setUploadedFile(file);
@@ -22,43 +26,69 @@ function Upload({
   function handleUpload() {
     fileInput.current?.click();
   }
+
+  function toggleAlert() {
+    setShowAlert((prev) => !prev);
+  }
+
   return (
     <div className="flex h-[70vh] flex-col items-center justify-center space-y-3">
-      <TextField
-        data-cy="dataset-id-field"
-        label="Dataset ID"
-        placeholder="dataset id"
-        required
-        type="text"
-        onChange={(event) => onUpdateSelectedRepo(event.target.value)}
-      />
-      {/*
-       * We have to use a button and an invisible input to trigger the file upload
-       * since MUI doesn't have a native file input.
-       * Once the button is clicked we click the invisible input through DOM manipulation
-       * to trigger the file upload.
-       */}
-      <label htmlFor="file-upload">
-        <input
-          ref={fileInput}
-          accept="*/*"
-          style={{ display: 'none' }}
-          id="file-upload"
-          type="file"
-          onChange={handleFileChange}
-        />
-        <Button
-          data-cy="upload-file-button"
-          onClick={() => handleUpload()}
-          startIcon={<CloudUploadIcon />}
-          variant="contained"
-        >
-          Upload File
-        </Button>
-      </label>
-      <Typography>{uploadedFile && `File uploaded: ${uploadedFile.name}`}</Typography>
+      <div className="flex w-1/2 items-center justify-center">
+        <IconButton component="button" color="primary" onClick={() => toggleAlert}>
+          <HelpIcon fontSize="medium" />
+        </IconButton>
+        <Collapse in={showAlert} className="ml-2 flex-grow">
+          <Alert severity="info">
+            Please fill in the following information, upload the data dictionary, and click submit.
+          </Alert>
+        </Collapse>
+      </div>
 
-      <Button data-cy="submit-button" variant="contained" onClick={() => onHandleSubmit()}>
+      <TextField className="w-1/2" label="User full name" required />
+      <TextField className="w-1/2" label="Email" required />
+      <TextField className="w-1/2" label="GitHub username" />
+      <TextField
+        className="w-1/2"
+        label="Summary of changes to the data dictionary"
+        placeholder={`- Added complete annotation for age and sex columns\n- Added partial annotation for diagnosis and assessment tool columns`}
+        multiline
+        minRows={10}
+        required
+      />
+
+      <div className="flex w-1/2 items-center justify-between">
+        <TextField
+          data-cy="dataset-id-field"
+          label="Dataset ID"
+          placeholder="dataset id"
+          required
+          type="text"
+          onChange={(event) => onUpdateSelectedRepo(event.target.value)}
+          className="flex-grow"
+        />
+
+        <label htmlFor="file-upload" className="ml-4">
+          <input
+            ref={fileInput}
+            accept="*/*"
+            style={{ display: 'none' }}
+            id="file-upload"
+            type="file"
+            onChange={() => handleFileChange}
+          />
+          <Button
+            data-cy="upload-file-button"
+            onClick={() => handleUpload}
+            startIcon={<CloudUploadIcon />}
+            variant="contained"
+          >
+            Upload File
+          </Button>
+        </label>
+      </div>
+
+      <Typography>{uploadedFile && `File uploaded: ${uploadedFile.name}`}</Typography>
+      <Button data-cy="submit-button" variant="contained" onClick={onHandleSubmit}>
         Submit
       </Button>
     </div>
