e101b8bdc99cb06741af8d44e15b9f4ff180e351
rmanaem <rmanaem@protonmail.ch>
2024-09-05 14:51:43 -0400

    Integrated `Upload` component in `NBDialog` as part of `RepoCard`

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -8,6 +8,9 @@ import CancelSharpIcon from '@mui/icons-material/CancelSharp';
 import DownloadIcon from '@mui/icons-material/Download';
 import UploadIcon from '@mui/icons-material/Upload';
 import axios from 'axios';
+import { VariantType } from 'notistack';
+import NBDialog from './NBDialog';
+import Upload from './Upload';
 
 const ORGURL = 'https://github.com/OpenNeuroDatasets-JSONLD/';
 const DOWNLOADURL = 'https://raw.githubusercontent.com/OpenNeuroDatasets-JSONLD/';
@@ -18,14 +21,16 @@ const RepoCard = memo(
     tsvExists,
     jsonExists,
     annotated,
-    onSomeError,
+    onSomeEvent,
   }: {
     repoName: string;
     tsvExists: boolean;
     jsonExists: boolean;
     annotated: boolean;
-    onSomeError: (error: string) => void;
+    onSomeEvent: (error: string, variant: VariantType) => void;
   }) => {
+    const [openUploadDialog, setUploadDialog] = useState(false);
+
     function downloadFile(fileBlob: Blob, fileName: string) {
       const blob = new Blob([fileBlob]);
       const downloadLink = document.createElement('a');
@@ -52,114 +57,124 @@ const RepoCard = memo(
             const response = await axios.get(url, { responseType: 'blob' });
             downloadFile(response.data, file);
           } catch (errorMain) {
-            onSomeError(`Failed to download file from main branch: ${errorMain}`);
+            onSomeEvent(`Failed to download file from main branch: ${errorMain}`, 'error');
           }
         } else {
-          onSomeError(`Failed to download file: ${error}`);
+          onSomeEvent(`Failed to download file: ${error}`, 'error');
         }
       }
     }
 
     return (
-      <Card data-cy={`card-${repoName}`}>
-        <CardContent>
-          <div className="grid grid-cols-4 items-center justify-items-center">
-            <div>
-              <Typography variant="h5">
-                <Button
-                  className="text-xl"
-                  href={`${ORGURL}${repoName}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {repoName}
+      <>
+        <Card data-cy={`card-${repoName}`}>
+          <CardContent>
+            <div className="grid grid-cols-4 items-center justify-items-center">
+              <div>
+                <Typography variant="h5">
+                  <Button
+                    className="text-xl"
+                    href={`${ORGURL}${repoName}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {repoName}
+                  </Button>
+                </Typography>
+                <Button endIcon={<UploadIcon />} onClick={() => setUploadDialog(true)}>
+                  Upload
                 </Button>
-              </Typography>
-              <Button endIcon={<UploadIcon />}>Upload</Button>
-            </div>
-            <div>
-              <Typography variant="subtitle2">
+              </div>
+              <div>
+                <Typography variant="subtitle2">
+                  {tsvExists ? (
+                    <Button
+                      href={`${ORGURL}${repoName}/tree/master/participants.tsv`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Participants.tsv
+                    </Button>
+                  ) : (
+                    <Button disabled>Participants.tsv</Button>
+                  )}
+                </Typography>
                 {tsvExists ? (
-                  <Button
-                    href={`${ORGURL}${repoName}/tree/master/participants.tsv`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Participants.tsv
-                  </Button>
+                  <div className="space-x-1">
+                    <CheckCircleSharpIcon color="success" />
+                    <DownloadIcon
+                      onClick={() => handleDownload('participants.tsv')}
+                      color="primary"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </div>
                 ) : (
-                  <Button disabled>Participants.tsv</Button>
+                  <div className="space-x-1">
+                    <CancelSharpIcon color="error" />
+                    <DownloadIcon color="disabled" />
+                  </div>
                 )}
-              </Typography>
-              {tsvExists ? (
-                <div className="space-x-1">
-                  <CheckCircleSharpIcon color="success" />
-                  <DownloadIcon
-                    onClick={() => handleDownload('participants.tsv')}
-                    color="primary"
-                    style={{ cursor: 'pointer' }}
-                  />
-                </div>
-              ) : (
-                <div className="space-x-1">
-                  <CancelSharpIcon color="error" />
-                  <DownloadIcon color="disabled" />
-                </div>
-              )}
-            </div>
-            <div>
-              <Typography variant="subtitle2">
+              </div>
+              <div>
+                <Typography variant="subtitle2">
+                  {jsonExists ? (
+                    <Button
+                      href={`${ORGURL}${repoName}/tree/master/participants.json`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Participants.json
+                    </Button>
+                  ) : (
+                    <Button disabled>Participants.json</Button>
+                  )}
+                </Typography>
                 {jsonExists ? (
-                  <Button
-                    href={`${ORGURL}${repoName}/tree/master/participants.json`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Participants.json
-                  </Button>
+                  <div className="space-x-1">
+                    <CheckCircleSharpIcon color="success" />
+                    <DownloadIcon
+                      onClick={() => handleDownload('participants.json')}
+                      color="primary"
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </div>
                 ) : (
-                  <Button disabled>Participants.json</Button>
+                  <div className="space-x-1">
+                    <CancelSharpIcon color="error" />
+                    <DownloadIcon color="disabled" />
+                  </div>
                 )}
-              </Typography>
-              {jsonExists ? (
-                <div className="space-x-1">
-                  <CheckCircleSharpIcon color="success" />
-                  <DownloadIcon
-                    onClick={() => handleDownload('participants.json')}
-                    color="primary"
-                    style={{ cursor: 'pointer' }}
-                  />
-                </div>
-              ) : (
-                <div className="space-x-1">
-                  <CancelSharpIcon color="error" />
-                  <DownloadIcon color="disabled" />
-                </div>
-              )}
-            </div>
-            <div>
-              <Typography variant="subtitle2">
+              </div>
+              <div>
+                <Typography variant="subtitle2">
+                  {annotated ? (
+                    <Button
+                      href={`https://github.com/neurobagel/openneuro-annotations/tree/main/${repoName}.jsonld`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Annotated
+                    </Button>
+                  ) : (
+                    <Button disabled>Annotated</Button>
+                  )}
+                </Typography>
                 {annotated ? (
-                  <Button
-                    href={`https://github.com/neurobagel/openneuro-annotations/tree/main/${repoName}.jsonld`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Annotated
-                  </Button>
+                  <CheckCircleSharpIcon color="success" />
                 ) : (
-                  <Button disabled>Annotated</Button>
+                  <CancelSharpIcon color="error" />
                 )}
-              </Typography>
-              {annotated ? (
-                <CheckCircleSharpIcon color="success" />
-              ) : (
-                <CancelSharpIcon color="error" />
-              )}
+              </div>
             </div>
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+        <NBDialog
+          open={openUploadDialog}
+          onClose={() => setUploadDialog(false)}
+          title={`Uploading data dictionary for ${repoName}`}
+          content={<Upload repoName={repoName} onSomeEvent={onSomeEvent} />}
+        />
+      </>
     );
   }
 );
