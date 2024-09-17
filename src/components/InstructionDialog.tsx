3379df0b647dbbd20f690818d35e60a8ad57b068
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-09-17 10:49:39 -0400

    [FIX] List nesting corrected

--- src/components/InstructionDialog.tsx
+++ src/components/InstructionDialog.tsx
@@ -24,17 +24,21 @@ function InstructionDialog({ open, onClose }: { open: boolean; onClose: () => vo
         <DialogContentText>
           Please follow the steps below
           <ol>
-            <li>Find the repository for the dataset you&apos;d like to work on</li>
-            <ul>
-              <li>
-                You can filter through the datasets using the name or status dataset and
-                availability of the files
-              </li>
-            </ul>
-            <li>Download the participant.tsv and/or the participants.json files</li>
-            <ul>
-              <li>You can do so by simply clicking on them in the repository card</li>
-            </ul>
+            <li>
+              Find the repository for the dataset you&apos;d like to work on
+              <ul>
+                <li>
+                  You can filter through the datasets using the name or status dataset and
+                  availability of the files
+                </li>
+              </ul>
+            </li>
+            <li>
+              Download the participant.tsv and/or the participants.json files
+              <ul>
+                <li>You can do so by simply clicking on them in the repository card</li>
+              </ul>
+            </li>
             <li>
               Once you have the files downloaded locally, head over to the{' '}
               <a href="https://annotate.neurobagel.org/" target="_blank" rel="noopener noreferrer">
